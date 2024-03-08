1a26130d7adb310629921bac901a4e4059a125a0
Kevser Yazici <mailkevseryazici@gmail.com>
2024-03-08 22:59:06 +0300

    appium_devam

--- src/test/java/tests/ApkYukleme.java
+++ src/test/java/tests/ApkYukleme.java
@@ -20,7 +20,7 @@ public class ApkYukleme {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\HUAWEI\\IdeaProjects\\Appium_T127\\Apps\\arabam.com_5.1.6_Apkpure.apk");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\HUAWEI\\IdeaProjects\\Appium_T127\\Apps\\Kiwi.com - Book Cheap Flights_2023.14.0_Apkpure (1).apk");
 
         driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
--- src/test/java/tests/ArabamApp.java
+++ src/test/java/tests/ArabamApp.java
@@ -4,6 +4,7 @@ import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 import org.aspectj.weaver.ast.And;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -13,6 +14,7 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.AssertJUnit.assertTrue;
@@ -47,7 +49,7 @@ public class ArabamApp {
         driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
-        // com.dogan.arabam.presentation.feature.home.HomeActivity -
+        // com.dogan.arabam.presentation.feature.home.HomeActivity
 
     }
 
@@ -55,31 +57,53 @@ public class ArabamApp {
     @Test
     public void arabamTest() throws InterruptedException {
         //driver.activateApp("com.dogan.arabam");
-
         // uygulamanin basarili bir sekilde yuklendigi dogrulanir
         assertTrue(driver.isAppInstalled("com.dogan.arabam"));
 
         // uygulaminin basarili bir sekilde acildigi dogrulanir
-        assertTrue(driver.findElementById("com.dogan.arabam:id/ivArabamLogo").isDisplayed());
+        assertTrue(driver.findElementById("com.dogan.arabam:id/ivArabamLogo").isDisplayed()); // uygulamanın logosunu
 
         // alt menuden ilan ara butonuna tiklanir
         driver.findElementByXPath("//*[@text='İlan Ara']").click();
+
         // kategori olarak otomobil secilir
-        //driver.findElementByXPath("//*[@text='Otomobil'").click();  // tıklama yapmak için kullanılan klasik yöntem
-        Thread.sleep(2000);
-        TouchAction action = new TouchAction<>(driver);
-        action.press(PointOption.point(994,500)) // ekranda tıklamam yapılacak en ve boy bilgilerinin girildiği kısım.
-                .release()// tıklama işlemi yaptıktan sonra tıklamanın gerçekleşmesi için ekranda parmağımızı kaldırma işlemi
-                .perform();// verilen action görevlerinin yerine getirmesi için action methodlarına verilen YAP(yerine getir) emri.
+
+        //driver.findElementByXPath("//*[@text='Otomobil']").click(); ---> tıklama işlemi için kullanılan klasik yöntem.
+
+        Thread.sleep(1000);
+        TouchAction action = new TouchAction<>(driver); // Hangi cihaz üzerinde çalışacaksak o cihaza ait driver
+
+        action.press(PointOption.point(994,500)) // Ekranda tıklama yapılacak en-boy bilgilerinin girldiği kısım. En-Boy (x,y) bilgileri Inspector'den geliyor.
+              .release() //tıklama işlemi yaptıktan sonra tıklamanın gerçekleşmesi için ekrandan parmağımızı kaldırma işlemi
+              .perform(); // verilen action görevlerinin yerine getirilmesi için action methodlarına verilen YAP (yerine getir) emri.
+
+
+        Thread.sleep(1000);
+
+        for(int i=0; i<5; i++){
+        action.press(PointOption.point(482,1516)) // kaydırma yapmak için ekranda belirlenecek nokta
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(600)))
+                .moveTo(PointOption.point(482,320))
+                .release()
+                .perform();
+        Thread.sleep(500);
+        }
 
         // arac olarak Volkswagen secilir
-
+        driver.findElementByXPath("//*[@text='Volkswagen']").click();
 
         // arac markasi olarak passat secilir
+        driver.findElementByXPath("//*[@text='Passat']").click();
 
         // 1.4 TSI BlueMotion secilir
+        driver.findElementByXPath("//*[@text='1.4 TSi BlueMotion']").click();
 
         // Paket secimi yapilir
+        // 412  721
+        action.press(PointOption.point(420,720))
+                .release()
+                .perform();
+
 
         // Ucuzdan pahaliya siralama yaparak filtreleme yapilir
         
